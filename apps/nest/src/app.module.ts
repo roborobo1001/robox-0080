9256c670b27e24025046234154f2129c5e323758
RoboRobo1001 <seann.10304@gmail.com>
2026-03-12 14:34:27 -0400

    Add Drizzle database setup and Nest DB module

--- apps/nest/src/app.module.ts
+++ apps/nest/src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './features/hello/hello.module';
-
+import { DrizzleModule } from './db/db.module';
+import { HelloModule } from './modules/hello/hello.module';
+import { UsersModule } from './modules/users/users.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    DrizzleModule,
     HelloModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
